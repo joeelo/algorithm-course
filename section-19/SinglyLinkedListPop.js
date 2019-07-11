8cae25f55da038e7ec96ba5918e8971255e3e8fd
Joseph <josephclorenzo@gmail.com>
2019-07-11 12:09:54 -0400

    pop and shift completed

---
+++ section-19/SinglyLinkedListPop.js
@@ -0,0 +1,57 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val)
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            let newTail = current;
+            current = current.next;
+            
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+}
+
+let list = new SinglyLinkedList();
+list.push("hello");
+list.push("goodbye");
+list.push("!");
+list.pop();
+console.log(list)
+
+// In order to pop something off the list you need to see if there are more values
+// While there is a
--- section-19/singlyLinkedList.js
+++ section-19/singlyLinkedList.js
@@ -1,5 +1,3 @@
-import { listenerCount } from "cluster";
-
 //SINGLY LINKED LIST
 
 // In an array each index is mapped, there is an order
@@ -18,22 +16,44 @@ import { listenerCount } from "cluster";
 class Node { 
     constructor(val) {
         this.val = val;
-        this.next = next;
+        this.next = null;
     }
 }
 
 class SinglyLinkedList {
     constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 
+    push(val) {
+        let newNode = new Node(val)
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
-let list = new SinglyLinkedList;
+
+let list = new SinglyLinkedList();
+
+list.push(7);
+console.log(list);
+
+
 // list.push("Hello!")
+// We don't initialize with any values with the exception of head and tail and both are null.
+// when pushing as long you as you put the value as the new tail nothing else needs to be changed.
 
-// let first = new Node("Hi");
-// first.next = new Node("there");
-// first.next.next = new Node ("how")
-// first.next.next.next = new Node ("are")
-// first.next.next.next.next = new Node ("you?")
 
-// We don't initialize with any 
+let first = new Node("Hi");
+first.next = new Node("there");
+first.next.next = new Node ("how")
+first.next.next.next = new Node ("are")
+first.next.next.next.next = new Node ("you?")
